8579492a8c324979232a255ecb34a47f0be90465
Jurgen Lamsens <jurgen.lamsens@telenet.be>
2026-09-11 10:11:02 +0200

    Update site 2026-09-11 10:11

--- go.sh
+++ go.sh
@@ -5,9 +5,11 @@ set -e
 if [ -z "$VIRTUAL_ENV" ]; then
   if [ -f ".venv/bin/activate" ]; then
     echo ">>> Virtual environment activeren..."
+    echo ""
     source .venv/bin/activate
   else
     echo ">>> Fout: Geen actieve .venv gevonden in deze map!"
+    echo ""
     exit 1
   fi
 fi
@@ -18,18 +20,26 @@ git add .
 # 3. Controleer of er effectief iets gewijzigd is in de broncode
 if [ -n "$(git status --porcelain)" ]; then
   echo ">>> Wijzigingen gevonden! Start build process..."
+  echo ""
   
   # Bouw de site met een schone cache
   zensical build --clean
+  echo ""
   
   echo ">>> Broncode committen en pushen naar main..."
+  echo ""
   git commit -m "Update site $(date +'%Y-%m-%d %H:%M')"
   git push origin main
+  echo ""
   
   echo ">>> Website deployen naar GitHub Pages..."
+  echo ""
   ghp-import -p -f site
+  echo ""
   
   echo ">>> Deploy succesvol afgerond!"
+  echo ""
 else
   echo ">>> Geen wijzigingen gevonden in de broncode. Niets om te bouwen of te pushen."
+  echo ""
 fi
